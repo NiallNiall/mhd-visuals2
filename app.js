88a0be4003f94337b38ff2137951e19cc6816773
NiallNiall <niall.henn@gmail.com>
2017-07-11 20:00:43 +0200

    new visuals

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+var port = 3000;
+http.listen(port, function() {
+	    console.log('Server running on port ' + port);
+});
+
+// Execute when a connection is made
+io.on('connection', function (socket) {
+
+	// Execute when draw is called from frontend
+	// Initialise a function called draw?
+	socket.on('draw', function (data) {
+		socket.emit('draw', data);
+	});
+
+});
+
+var startNum = 0;
+
+setInterval( function(){
+	startNum +=1;
+	socket.emit('draw', startNum);
+}, 1000);
+
+
+// io.on('connection', function(socket){
+//   console.log('A user connected');
+
+//   //Send a message after a timeout of 4seconds
+//   setTimeout(function(){
+// 		console.log('yoyo')
+// 	}, 4000);
+//   socket.on('disconnect', function () {
+//     console.log('A user disconnected');
+//   });
+// });
+
+
+
+
+// io.on('connection', function (socket) {
+// 	socket.on('draw', function (data) {
+// 		socket.broadcast.emit('draw', data);
+// 	});
+// });
+
+
+// var wscrl = 0;
+
+// setInterval(function () {
+// 	// console.log('boo');
+// 	if(wscrl < 3){
+// 		wscrl +=1;
+// 	} else {
+// 		wscrl = 0;
+// 	}
+// 	console.log(wscrl);
+
+// 	io.sockets.emit('users_count', clients);
+
+// }, 1000);
+
---
+++ public/script.js
@@ -0,0 +1,122 @@
+function sendName(sentName){
+	console.log(sentName);
+
+	document.getElementById("texter").innerHTML = "Paragraph changed!";
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var socket = io.connect();
+
+	// // This gets sent to all
+ //  	socket.emit('draw', {
+	//     'Name': 'frank'
+	//  });
+
+  	window.onmousemove = function (e) {
+  		console.log('sss');
+
+  		var nameTest = 'frank';
+
+  		// This gets sent to all
+	  	socket.emit('draw', {
+		    'Name': nameTest
+		 });
+
+	  	// sendName(nameTest);
+
+	}
+
+  	// This gets received from all
+	socket.on('draw', function(data) {
+	  sendName(data.Name);
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+// var wscrl = 0;
+
+// var words = ['big', 'small', 'dog', 'cat'];
+
+// var textCont = document.getElementById("texter");
+
+// textCont.innerHTML = words[wscrl];
+
+// window.setInterval(function () {
+// 		textCont.innerHTML = words[wscrl];
+
+// 		if(wscrl < 3){
+// 			wscrl +=1;
+// 		} else {
+// 			wscrl = 0;
+// 		}
+
+//     }, 500);
+
+
+// function drawLine(context, x1, y1, x2, y2) {
+// 	context.moveTo(x1, y1);
+// 	context.lineTo(x2, y2);
+// 	context.stroke();
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+// 	var canvas = document.getElementById('canvas');
+// 	var context = canvas.getContext('2d');
+// 	var width = window.innerWidth;
+// 	var height = window.innerHeight;
+
+// 	canvas.width = width;
+// 	canvas.height = height;
+
+// 	var drawing = false;
+// 	var x, y, prevX, prevY;
+
+// 	var socket = io.connect();
+
+// 	canvas.onmousedown = function(e) {
+// 		drawing = true;
+// 		prevX = x;
+// 		prevY = y;
+// 	}
+
+// 	canvas.onmouseup = function(e) {
+// 		drawing = false;
+// 	}
+
+// 	canvas.onmousemove = function(e) {
+// 		x = e.clientX;
+// 		y = e.clientY;
+// 		if (drawing) {
+// 			socket.emit('draw', {
+// 				'x1': prevX,
+// 				'y1': prevY,
+// 				'x2': x,
+// 				'y2': y
+// 			});
+
+// 			drawLine(context, prevX, prevY, x, y);
+// 			prevX = x;
+// 			prevY = y;
+// 		}
+// 	}
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+
+// 	socket.on('draw', function(data) {
+		
+// 	});
+// });
